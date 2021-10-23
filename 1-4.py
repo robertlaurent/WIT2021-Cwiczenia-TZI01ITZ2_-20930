48670ff010d208ae4d31e89e1fa76233d6848371
robertlaurent <92957074+robertlaurent@users.noreply.github.com>
2021-10-23 21:45:10 +0200

    Add files via upload

---
+++ 1-4.py
@@ -0,0 +1,223 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+5**-100/4
+3.1691265005705736e-71
+(5**3-100)/4
+6.25
+(15%4)+6
+9
+(2**2)+(24//4)
+10
+type (6)
+<class 'int'>
+type (6.0)
+<class 'float'>
+type (5 +3.14)
+<class 'float'>
+int (7.999)
+7
+float(6)
+6.0
+x=2
+x
+2
+x
+2
+x
+2
+1+x
+3
+x=3
+1.0+x
+4.0
+x=4
+x
+4
+1st_number=1
+SyntaxError: invalid decimal literal
+1st+number=1
+SyntaxError: invalid decimal literal
+(1st_number)=1
+SyntaxError: invalid decimal literal
+first_number=1
+ amount_in_$=10
+ 
+SyntaxError: unexpected indent
+ amount_in_dolars=10
+ 
+SyntaxError: unexpected indent
+ Amount_in_$=10
+ 
+SyntaxError: unexpected indent
+
+amount_in$=10
+SyntaxError: invalid syntax
+amount_ind_$=10
+SyntaxError: invalid syntax
+x=3
+y=5
+x=3
+x
+3
+y=5
+y
+5
+x+x
+6
+x+x-(2**2)
+2
+x, y= 8, 5
+x, y
+(8, 5)
+print (x)
+8
+print (y)
+5
+bok1=3
+bok2=4
+bok1
+3
+bok2
+4
+bok1**2+bok2**2=bok3**2
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+Bok1**2+bok2**2=bok3**2
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+(bok1)**2+(bok2)**2=bok3**2
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+bok1
+3
+bok2
+4
+
+bok1**2
+9
+bok1**2+bok2**2=
+SyntaxError: cannot assign to expression
+bok1**2+book2**2
+Traceback (most recent call last):
+  File "<pyshell#56>", line 1, in <module>
+    bok1**2+book2**2
+NameError: name 'book2' is not defined. Did you mean: 'bok2'?
+"bok1"**2
+Traceback (most recent call last):
+  File "<pyshell#57>", line 1, in <module>
+    "bok1"**2
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+bok1**2
+9
+bok1**2+ bok2**2
+25
+bok1**2+ bok**2= bok3**2
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?bok1**2+ bok**2 = bok3**2
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+bok1**2+ bok2**2
+25
+bok3= 5
+bok 3
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+bok3
+5
+bok2
+4
+bok1
+3
+powitanie= 'hello'
+powitanie
+'hello'
+print(powitanie+'user')
+hellouser
+hellouser
+Traceback (most recent call last):
+  File "<pyshell#71>", line 1, in <module>
+    hellouser
+NameError: name 'hellouser' is not defined
+print(5*powitanie)
+hellohellohellohellohello
+driver='Norris'
+brand= 'Mclaren'
+print(f”{driver}  drives  for  {brand}”)oraz  print(“{}drives  for {}”.format(driver, brand))
+SyntaxError: invalid character '”' (U+201D)
+ print(f”{driver}  drives  for  {brand}”)
+ 
+SyntaxError: unexpected indent
+ print(f”{driver}  drives  for  {brand}”)
+ 
+SyntaxError: unexpected indent
+ print(f”{driver}  drives  for  {brand}”)
+ 
+SyntaxError: unexpected indent
+ print(f”{driver}
+       
+SyntaxError: unexpected indent
+len(driver(
+    len(driver)
+
+
+    
+    len(brand)
+
+     driver.lower()
+    driver.upper()
+    driver.capitalize()
+     driver.count(‘r’)
+    
+SyntaxError: invalid character '‘' (U+2018)
+driver[1]
+    
+'o'
+brand[3]
+    
+'a'
+ driver[-1]
+    
+SyntaxError: unexpected indent
+driver[-1]
+    
+'s'
+brand[-3]
+    
+'r'
+driver[1:3]
+    
+'or'
+brand [1:-1]
+    
+'clare'
+brands=‘Mrcedes, Red Bull, McLaren’
+    
+SyntaxError: invalid character '‘' (U+2018)
+brands= ‘Mrcedes, Red Bull, McLaren’
+    
+SyntaxError: invalid character '‘' (U+2018)
+
+‘Mrcedes, Red Bull, McLaren’
+    
+SyntaxError: invalid character '‘' (U+2018)
+brand=‘Mrcedes, Red Bull, McLaren’
+    
+SyntaxError: invalid character '‘' (U+2018)
+
+brands
+    
+Traceback (most recent call last):
+  File "<pyshell#102>", line 1, in <module>
+    brands
+NameError: name 'brands' is not defined. Did you mean: 'brand'?
+brands=1
+    
+1
+    
+1
+brands
+    
+1
+brands='Mrcedes, Red Bull, McLaren'
+    
+brands.replace(‘Mrcedes’, ‘Mercedes’)
+    
+SyntaxError: invalid character '‘' (U+2018)
+brands.replace('Mrcedes', 'Mercedes')
+    
+'Mercedes, Red Bull, McLaren'
